d1281463f77ccac492a648474113124e20284bac
karendelvalle <karendelvalle2006@gmail.com>
2021-08-17 11:38:08 -0400

    database original

--- proyectoIndividual/settings.py
+++ proyectoIndividual/settings.py
@@ -76,15 +76,10 @@ WSGI_APPLICATION = 'proyectoIndividual.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'first_postgres_db',
-        'USER': 'postgres',
-        'PASSWORD': '120587',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
